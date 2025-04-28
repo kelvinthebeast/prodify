fab2958cc73830f128631b8743cefbee487f69c3
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-04-28 18:25:46 +0700

    config db onlne

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 module.exports.connectDb = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/prodify");
+    await mongoose.connect(process.env.MONGO_URL);
     console.log("MongoDB connected successfully");
+    
   } catch (error) {
     console.error("MongoDB connection error:", error);   
   }
