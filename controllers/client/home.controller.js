963db072ee245e25bdd3718247583ad21eb95d3f
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-07 22:14:39 +0700

    add sub menu in client

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,4 +1,13 @@
-module.exports.index = (req, res) => { 
+
+// const ProductCategory = require('../../models/product-category.model')
+// const createTreeHelper = require('../../helpers/createTree')
+
+module.exports.index = async (req, res) => { 
+  // const productsCategory = await ProductCategory.find({
+  //   deleted: false
+  // })
+
+  // const newProductsCategory = createTreeHelper.tree({productsCategory})
   res.render("client/pages/home/index", { pageTitle: "Home" });
 }
 
