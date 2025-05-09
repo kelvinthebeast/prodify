41293d0e494e0485581ba1367736ee0d2d67bfcb
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-09 10:40:59 +0700

    demo

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,14 +1,17 @@
 
 // const ProductCategory = require('../../models/product-category.model')
 // const createTreeHelper = require('../../helpers/createTree')
-
+const productsHelper = require("../../helpers/product");
 module.exports.index = async (req, res) => { 
-  // const productsCategory = await ProductCategory.find({
-  //   deleted: false
-  // })
+  const products = await Product.find({
+    deleted: false,
+    featured: "1",
+    status: 'active'
+  }).sort({position: "desc"});
 
-  // const newProductsCategory = createTreeHelper.tree({productsCategory})
-  res.render("client/pages/home/index", { pageTitle: "Home" });
+  const newProducts = productsHelper.priceNewProducts(products);
+  console.log(products)
+  res.render("client/pages/home/index", { pageTitle: "Home", products: newProducts });
 }
 
 module.exports.create = (req, res) => {
