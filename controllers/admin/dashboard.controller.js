8e94d1f6060d9550b1f849d4040dec3086b95d46
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-05-15 13:42:19 +0700

    add dashboard admin

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,5 +1,41 @@
+const ProductCategory = require("../../models/product-category.model");
 module.exports.index = (req, res) => { 
   res.render("admin/pages/dashboard/index", { pageTitle: "Dashboard" });
+}
+
+module.exports.dashboard = async (req, res) => {
+    const statistic = {
+        categoryProduct: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        product: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+        account: {
+            total: 0,
+            inactive: 0,
+        },
+        user: {
+            total: 0,
+            active: 0,
+            inactive: 0,
+        },
+    };
+
+    statistic.categoryProduct.total = await ProductCategory.countDocuments({deleted: false});
+    statistic.categoryProduct.active = await ProductCategory.countDocuments({deleted: false, status: true});
+    statistic.categoryProduct.inactive = await ProductCategory.countDocuments({deleted: false, status: false});
+    res.render("admin/pages/dashboard/index.pug", {
+        
+        pageTitle: "Trang chủ",
+        statistic: statistic
+
+    });
+
 }
 
 module.exports.create = (req, res) => {
