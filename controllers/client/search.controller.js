1045348d0c7279740a91ec414d5ee188c54d5372
Thành Nhân <117796054+kelvinthebeast@users.noreply.github.com>
2025-06-19 22:14:13 +0700

    hotfix/fix search client

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,4 +1,5 @@
 const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
 module.exports.index = async (req, res) => {
   const keyword = req.query.keyword;
 
@@ -16,9 +17,11 @@ module.exports.index = async (req, res) => {
     })
 
     newProducts = products.map((item) => {
-      item.priceNew = (item.price - (item.price * item.discount) / 100).toFixed(0);
+      item.priceNew = (item.price - (item.price * item.discountPercentage) / 100).toFixed(0);
       return item;
-    })
+    });
+
+    
   }
   res.render("client/pages/products/index", {
     pageTitle: "Products",
